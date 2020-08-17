fc30706812cfab5faef3e1e69ad12b6f653dccd3
Ben <benclarke023@gmail.com>
2020-08-17 20:10:14 +0100

    Initial Commit

---
+++ Meme Creator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meme Creator
+//
+//  Created by Ben Clarke on 17/08/2020.
+//  Copyright © 2020 Ben Clarke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
